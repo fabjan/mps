8fddd687ad7aa7a0f128355bbfe6e9e39b051a55
Fabian <fabian@kejsarensnyaklader.se>
2016-12-20 23:39:54 +0100

    make bots do more

--- bot.sh
+++ bot.sh
@@ -1,8 +1,8 @@
 HOST=127.0.0.1
-PORT=52259
+PORT=80
 
-test "$1" && HOST=$1
-test "$2" && PORT=$2 
+test "$1" && PORT=$1
+test "$2" && HOST=$2
 
 function maybe {
   echo $(($RANDOM % 2 * $1))
@@ -13,7 +13,8 @@ echo "spamming commands to $HOST:$PORT"
 while true
 do
   DPAD=$(( `maybe 1` + `maybe 2` +  `maybe 4` + `maybe 8`))
-  INPUT="$DPAD"
+  BUTTONS=$(( `maybe 16` + `maybe 32` + `maybe 64` + `maybe 128` ))
+  INPUT=$(( $DPAD + $BUTTONS ))
   echo "$INPUT"
   sleep 0.1
 done | nc $HOST $PORT
